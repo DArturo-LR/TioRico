d647a87d0bb90ddce6b7bcf6154c9f14190d48a0
DArturo-LR <die.art2006@gmail.com>
2026-04-07 13:11:40 -0500

    Cambios para la prueba de login con el firebase

---
+++ app/src/main/java/com/example/tiorico/ui/login/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.tiorico.ui.login
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiorico.R
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val pass = etPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                login(email, pass)
+            } else {
+                Toast.makeText(this, "Completa los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnRegister.setOnClickListener {
+            val email = etEmail.text.toString()
+            val pass = etPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                register(email, pass)
+            } else {
+                Toast.makeText(this, "Completa los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun login(email: String, pass: String) {
+        auth.signInWithEmailAndPassword(email, pass)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun register(email: String, pass: String) {
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Usuario creado", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Error al registrar", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
